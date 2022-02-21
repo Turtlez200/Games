44aa5540fcf46eb7f91c2230a23515da611dabbb
Turtlez200 <88110937+Turtlez200@users.noreply.github.com>
2022-02-20 21:10:15 -0500

    Create TicTacToe.py

---
+++ TicTacToe.py
@@ -0,0 +1,26 @@
+import pygame
+import sys
+from pygame.locals import *
+import random
+
+pygame.init()
+
+WIDTH = 600
+HEIGHT = 600
+running = True
+clock = pygame.time.Clock()
+window = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
+pygame.display.set_caption('Tic Tac Toe')
+back = pygame.Surface((WIDTH, HEIGHT))
+background = back.convert()
+background.fill((0, 0, 0))
+basic_font = pygame.font.SysFont(None, 48)
+small = pygame.font.SysFont(None, 35)
+
+pygame.display.update()
+
+while running:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+            sys.exit()
